adad5f02280dd133f25f174201a518b898bbfad0
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2019-05-18 01:37:32 +0200

    intial version

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useCallback} from 'react';
+
+const HEX_MIN = 0;
+const HEX_MAX = 255;
+const randomHex = (): string => {
+  return (Math.floor(Math.random() * HEX_MAX) + HEX_MIN).toString(16);
+}
+const randomColor = (): string => {
+  const r = randomHex();
+  const g = randomHex();
+  const b = randomHex();
+
+  return `#${r}${g}${b}`;
+}
+
+const INITIAL_COLOR = randomColor();
 
 const App: React.FC = () => {
+  const [backgroundColor, setBackgroundColor] = useState(INITIAL_COLOR);
+
+  const handleClick = useCallback(() => {
+      setBackgroundColor(randomColor())
+  }, [setBackgroundColor]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button style={{
+      backgroundColor,
+    }}
+    onClick={handleClick}>
+      Baby colors
+    </button>
   );
 }
 
