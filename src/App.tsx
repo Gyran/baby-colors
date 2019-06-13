2bc72f3a496f2e209eeec7ccf0df2b1f599cf8f0
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2019-06-13 20:49:01 +0200

    feat: support for mouse and touch

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
-import React, {useState, useCallback} from 'react';
+import React, { useState, useCallback } from 'react';
 
 const INITIAL_COLOR = 'white';
 
 const App: React.FC = () => {
   const [backgroundColor, setBackgroundColor] = useState(INITIAL_COLOR);
+  const [canUseTouch, setCanUseTouch] = useState(false);
 
-  const handleTouchStart = useCallback(async () => {
+  const setRandomColor = useCallback(async () => {
     const { randomColor } = await import('./randomColor');
     setBackgroundColor(randomColor())
-  }, [setBackgroundColor]);
+  }, [setBackgroundColor])
+
+  const handleTouchStart = useCallback((e) => {
+    setCanUseTouch(true)
+    setRandomColor()
+  }, [setRandomColor]);
+
+  const handleOnMouseDown = useCallback(() => {
+    if (!canUseTouch) {
+      setRandomColor()
+    }
+  }, [canUseTouch, setRandomColor]);
 
   return (
-    <button 
+    <button
       style={{
         backgroundColor,
       }}
       onTouchStart={handleTouchStart}
+      onMouseDown={handleOnMouseDown}
     >
-      Baby colors testing
+      Baby colors
     </button>
   );
 }
